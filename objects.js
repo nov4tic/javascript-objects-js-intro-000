1caf1cb9547343ca5b4f92585a8db2b7577502e1
Dev Frias <ddfrias14@gmail.com>
2021-02-19 16:13:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {
-  Kendrick: 'Swimming pools';
-  Drake: 'Hotline Bling';
-  Lil Yachty: "Oprah's Bank Account";
+  Kendrick: 'Swimming pools',
+  Drake: 'Hotline Bling',
+  Lil Yachty: "Oprah's Bank Account",
 }
